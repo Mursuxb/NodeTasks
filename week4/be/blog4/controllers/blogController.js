07a3a00ad8ed2ba3cd529da342e9096b068fb8a3
Mursuxb <mursuxb@gmail.com>
2023-11-17 10:48:40 +0200

    Week 4 activities added

---
+++ week4/be/blog4/controllers/blogController.js
@@ -0,0 +1,119 @@
+const Blog = require("../models/blogModel");
+
+// get all blogs
+const getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.status(200).json({
+      status: "success",
+      results: blogs.length,
+      data: {
+        blogs,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+// get a single blog
+const getBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        blog,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+// create a new blog
+const createBlog = async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        blog: newBlog,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+// delete a blog
+const deleteBlog = async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+// Update blog using PATCH
+const patchBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }// To return the updated document
+    );
+
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+
+    res.json(blog);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// Update blog using PUT
+const putBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findOneAndReplace(
+      { _id: req.params.id },
+      req.body,
+      { new: true }// To return the updated document
+    );
+
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+
+    res.json(blog);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  getBlogs,
+  getBlog,
+  createBlog,
+  deleteBlog,
+  putBlog,
+  patchBlog,
+};
--- week4/fe/react-router-example/src/App.js
+++ week4/fe/react-router-example/src/App.js
@@ -4,18 +4,23 @@ import Navigation from './components/Navigation';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+import Layout from './components/Layout';
+
 
 function App() {
-return (
- <BrowserRouter>
-<Navigation />
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/about" element={<About />} />
-<Route path="/contact" element={<Contact />} />
-</Routes>
- </BrowserRouter>
-);
-}
-
-export default App;
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="About" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
+  export default App;
