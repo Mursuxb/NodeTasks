e9615c28cda565d7aab265aef1d7a179992049c3
Mursuxb <mursuxb@gmail.com>
2023-12-03 23:08:03 +0200

    corrected week number in journal

---
+++ week6/fe/custom-hook-lab/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SingleCounter from './SingleCounter'; // Update the import path as needed
+import './App.css'; // Import your CSS file for styling
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <SingleCounter />
+      <SingleCounter />
+      <SingleCounter />
+    </div>
+  );
+};
+
+export default App;
